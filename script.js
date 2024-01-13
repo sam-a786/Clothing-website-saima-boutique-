0db67c06f201c42c7265a7283b22dfab526fdb58
c2040142 <Nimsam786>
2024-01-13 14:31:53 +0000

    add basic homepage with navigation bar and footer

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get the button element by its ID
+var myButton = document.getElementById("myButton");
+
+// Add a click event listener to the button
+myButton.addEventListener("click", function() {
+    // Display an alert when the button is clicked
+    alert("Button clicked!");
+});
